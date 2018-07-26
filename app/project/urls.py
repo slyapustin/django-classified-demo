b4c4a0a6f30f65abd696935393e406f3df357c27
sergey <inoks@mail.ru>
2018-07-26 23:47:33 +0200

    Optimized imports

--- app/project/urls.py
+++ app/project/urls.py
@@ -1,9 +1,9 @@
+from django.conf import settings
 from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.views.generic import TemplateView
-from django.conf import settings
 
 urlpatterns = [
     url('', include('django_classified.urls', namespace='django_classified')),
