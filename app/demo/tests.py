b46df05cbac3a51b9b0c8c1ae7e81a5e846545a2
sergey <inoks@mail.ru>
2019-04-16 18:48:22 +0200

    Added missing init file and simple test.

--- app/demo/tests.py
+++ app/demo/tests.py
@@ -1,21 +1,7 @@
-from django.contrib.auth.models import User
 from django.test import TestCase
-from django.urls import reverse_lazy
 
 
-class AuthTestCase(TestCase):
-    def setUp(self):
-        self.username = 'john@example.com'
-        self.email = 'john@example.com'
-        self.password = 'qweqweqwe'
-
-    def create_user(self):
-        User.objects.create_user(username=self.username, email=self.email, password=self.password)
-
-    def test_email_login_page(self):
-        self.create_user()
-        login_page = reverse_lazy('login')
-        response = self.client.post(login_page, {'email': self.email, 'password': self.password}, follow=True)
+class DemoTestCase(TestCase):
+    def test_main_page(self):
+        response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.context, dict())
-        self.assertTrue(response.context['user'].is_authenticated)
